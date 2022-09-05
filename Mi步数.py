a5170a132c75da76105cd614beeb2f3400b99a53
TaichiYLY <98798684+TaichiYLY@users.noreply.github.com>
2022-09-05 10:46:57 +0800

    Create Mi步数.py

---
+++ Mi步数.py
@@ -0,0 +1,27 @@
+"""
+小米步数接口
+
+Version: 0.1
+Author: Taichi
+"""
+import requests
+
+def html_post(url, phone, password, num):
+    data = requests.post(url=url,params={
+        'phone': phone,
+        'pwd': password,
+        'num': num,
+    })
+    print(data.json())
+
+if __name__ == '__main__':
+    # 刷步数 API
+    url = 'https://motion.faithxy.com/motion/api/motion/Xiaomi'
+    # 账号
+    phone = '17531331461'
+    # 密码
+    password = 'yly20040224'
+    # 步数
+    size = '23000'
+    # 调用函数
+    html_post(url=url, phone=phone, password=password, num=size)
